be0af52d4b867a9e901f035c64e9ea1ac8e799c2
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-11 12:24:45 +0630

    refactoring

---
+++ src/app/versioning/versioning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-versioning',
+  templateUrl: './versioning.component.html',
+  styleUrls: ['./versioning.component.scss']
+})
+export class VersioningComponent {
+
+}
--- src/app/versioning/versioning.module.ts
+++ src/app/versioning/versioning.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationManagerComponent } from './components/notification-manager/notification-manager.component';
-import { VersioningComponent } from './versioning/versioning.component';
+import { VersioningComponent } from './versioning.component';
 
 
 
 @NgModule({
   declarations: [
     NotificationManagerComponent,
-    VersioningComponent
+    VersioningComponent,
   ],
   imports: [
     CommonModule
