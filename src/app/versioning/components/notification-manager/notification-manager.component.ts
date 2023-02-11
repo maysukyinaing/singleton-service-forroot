48324ba7e2a721d68f9db2223a633faae444fbc1
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-11 12:21:52 +0630

    versioning module

---
+++ src/app/versioning/components/notification-manager/notification-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-manager',
+  templateUrl: './notification-manager.component.html',
+  styleUrls: ['./notification-manager.component.scss']
+})
+export class NotificationManagerComponent {
+
+}
--- src/app/versioning/versioning.module.ts
+++ src/app/versioning/versioning.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotificationManagerComponent } from './components/notification-manager/notification-manager.component';
+import { VersioningComponent } from './versioning/versioning.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotificationManagerComponent,
+    VersioningComponent
+  ],
   imports: [
     CommonModule
   ]
