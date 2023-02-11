cada597766b7406f8d5263502f42fb97f8101a8c
MaySuKyiNaing <maysukyinaing@gmail.com>
2023-02-11 12:37:02 +0630

    routing module

--- src/app/versioning/versioning.module.ts
+++ src/app/versioning/versioning.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationManagerComponent } from './components/notification-manager/notification-manager.component';
 import { VersioningComponent } from './versioning.component';
+import {VersioningRoutingModules} from "./versioning-routing.modules";
 
 
 
@@ -11,7 +12,7 @@ import { VersioningComponent } from './versioning.component';
     VersioningComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,VersioningRoutingModules
   ]
 })
 export class VersioningModule { }
